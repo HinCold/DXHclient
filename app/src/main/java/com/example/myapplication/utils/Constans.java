d944c6db1f766254c2b49c8494bf7f57b7a1adcb
XPR <984822045@qq.com>
2023-10-23 19:27:39 +0800

    ready to create constans and bean;

---
+++ app/src/main/java/com/example/myapplication/utils/Constans.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.utils;
+
+public class Constans {
+    public static final String WEB_SITE="http://127.0.0.1/promotion";
+    public static final String REQUEST_SHOP_DATA = "shop_list_data.json";
+
+}
